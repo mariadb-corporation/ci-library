cfe497e3176200891f9fbcc227c419a3410c747c
Alexey Bychko <abychko@gmail.com>
2020-06-22 23:16:45 +0700

    galera installation fix

--- vars/PrepareThisBuild.groovy
+++ vars/PrepareThisBuild.groovy
@@ -11,7 +11,7 @@ def call() {
   currentBuild.description = "Revision: ${GIT_COMMIT}"
 
   currentVersion = readProperties file: 'VERSION', interpolate: true
-  minorVersion = currentVersion.['MYSQL_VERSION_MINOR']
+  minorVersion = currentVersion['MYSQL_VERSION_MINOR']
   shortVersion = currentVersion['MYSQL_VERSION_MAJOR'] + '.' + currentVersion['MYSQL_VERSION_MINOR']
   fullVersion = shortVersion + '.' + currentVersion['MYSQL_VERSION_PATCH'] + currentVersion['MYSQL_VERSION_EXTRA']
   serverMaturity = currentVersion['SERVER_MATURITY']
