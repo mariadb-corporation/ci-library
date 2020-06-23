d8e1575cb1be1297046a82711211e1bac148685b
Alexey Bychko <abychko@gmail.com>
2020-06-23 11:51:13 +0700

    env file fix

--- vars/PrepareThisBuild.groovy
+++ vars/PrepareThisBuild.groovy
@@ -10,10 +10,12 @@ def call() {
   currentBuild.displayName = "#${BUILD_NUMBER}: ${GIT_BRANCH}"
   currentBuild.description = "Revision: ${GIT_COMMIT}"
 
-  currentVersion = readProperties file: 'VERSION', interpolate: true
-  minorVersion = currentVersion['MYSQL_VERSION_MINOR']
-  shortVersion = currentVersion['MYSQL_VERSION_MAJOR'] + '.' + currentVersion['MYSQL_VERSION_MINOR']
-  fullVersion = shortVersion + '.' + currentVersion['MYSQL_VERSION_PATCH'] + currentVersion['MYSQL_VERSION_EXTRA']
-  serverMaturity = currentVersion['SERVER_MATURITY']
+  currentVersion = readProperties file: 'VERSION'
+  minorVersion = currentVersion.MYSQL_VERSION_MINOR
+  shortVersion = currentVersion.MYSQL_VERSION_MAJOR + '.' + currentVersion.MYSQL_VERSION_MINOR
+  fullVersion = shortVersion + '.' + currentVersion.MYSQL_VERSION_PATCH + currentVersion.MYSQL_VERSION_EXTRA
+  serverMaturity = currentVersion.SERVER_MATURITY
+
+  echo "Minor version is ${minorVersion}"
 
 }
